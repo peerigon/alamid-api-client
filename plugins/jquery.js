54588d07d05905b31f0e24b414fbc618c312e1fe
Johannes Ewald <johannes.ewald@peerigon.com>
2014-11-07 17:44:14 +0100

    Add first shot of routes

--- plugins/jquery.js
+++ plugins/jquery.js
@@ -9,29 +9,31 @@ function jqueryPlugin(api, config) {
     config.settings = config.settings || {};
     config.settings.contentType = config.settings.contentType || "application/json; charset=utf-8";
 
-    function request(method, url, data, callback) {
-        var settings = obj.copy(config.settings);
+    api.transports.push({
+        name: "http",
+        deliver: function transport(method, url, data, callback) {
+            var settings = obj.copy(config.settings);
 
-        settings.type = method.toUpperCase();
-        if (data !== null) {
+            settings.type = method.toUpperCase();
             if (method !== "get" && settings.contentType.indexOf("application/json") > -1) {
                 data = JSON.stringify(data);
             }
             settings.data = data;
+
+            $.ajax(base + url, settings)
+                .done(function onHttpSuccess(data) {
+                    callback(null, data);
+                })
+                .fail(function onHttpFail(xhr) {
+                    callback(new Error(xhr.responseText));
+                });
+
+            return true;
+        },
+        isAvailable: function () {
+            return true;    // http is always available, Dude!
         }
-
-        $.ajax(base + url, settings)
-            .done(function onHttpSuccess(data) {
-                callback(null, data);
-            })
-            .fail(function onHttpFail(xhr) {
-                callback(new Error(xhr.responseText));
-            });
-
-        return true;
-    }
-
-    api.transports.push(request);
+    });
 }
 
 module.exports = jqueryPlugin;
